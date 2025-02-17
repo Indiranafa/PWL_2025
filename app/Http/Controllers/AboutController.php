9ade60fde1238d721053dabbffa38a43c8c7e593
Indiranafa <indiranafa24@gmail.com>
2025-02-17 11:15:56 +0700

    add: controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return 'Nama: Indira Nafa Aurah Huda, NIM: 2341720001';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,13 +25,13 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return 'NIM: 2341720001, Nama: Indira Nafa Aurah Huda';
-});
+// Route::get('/about', function () {
+//     return 'NIM: 2341720001, Nama: Indira Nafa Aurah Huda';
+// });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
@@ -37,11 +41,29 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' .$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID ' . $id;
+// });
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya ' . $name;
 });
 
+//WelcomeController
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+//PageController
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']); 
+
+//HomeController
+Route::get('/', [HomeController::class,'index']);
+//AboutController
+Route::get('/about', [PageController::class,'about']);
+//ArticlesController
+Route::get('/articles/{id}', [PageController::class, 'articles']); 
+// PhotoController
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
